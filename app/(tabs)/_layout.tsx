58595d0e5d333efd677a26deeab79519f09f8653
danny1998cuba <danny.glezcuet98@gmail.com>
2026-07-03 12:04:59 -0400

    chore: build adjustments

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,79 @@
+import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { useTheme } from "@/hooks/use-theme";
+import AppProviders from "@/lib/providers/AppProviders";
+import { ChartColumn, Ellipsis, Route, Settings } from "lucide-react-native";
+
+export default function RootLayout() {
+  const { colors } = useTheme();
+
+  return (
+    <AppProviders>
+      <Tabs
+        screenOptions={{
+          tabBarInactiveTintColor: colors.tabIconDefault,
+          tabBarActiveTintColor: colors.tabIconSelected,
+          tabBarStyle: {
+            backgroundColor: colors.surface,
+            borderTopColor: colors.border,
+            borderTopWidth: 1,
+          },
+
+          sceneStyle: {
+            backgroundColor: colors.background,
+          },
+
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen name="index" options={{ href: null }} />
+        <Tabs.Screen
+          name="trips"
+          options={{
+            title: "Trips",
+            popToTopOnBlur: true,
+            tabBarIcon: ({ color, size }) => (
+              <Route color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="reports"
+          options={{
+            title: "Reports",
+
+            tabBarIcon: ({ color, size }) => (
+              <ChartColumn color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="more"
+          options={{
+            title: "More",
+            popToTopOnBlur: true,
+            tabBarIcon: ({ color, size }) => (
+              <Ellipsis color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+
+            tabBarIcon: ({ color, size }) => (
+              <Settings color={color} size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+      <StatusBar style="auto" />
+    </AppProviders>
+  );
+}
